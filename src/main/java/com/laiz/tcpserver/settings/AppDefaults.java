7a070e37699e1c470d3988d0719ccf8c8891c48e
Home PC <example@mail.com>
2025-03-26 23:41:54 +0300

    refactored AppDefaults

--- src/main/java/com/laiz/tcpserver/settings/AppDefaults.java
+++ src/main/java/com/laiz/tcpserver/settings/AppDefaults.java
@@ -7,7 +7,7 @@ public class AppDefaults {
     public static final int SO_TIMEOUT = 3000;
     public static final int PORT = 2404;
     public static final byte START_BYTE = 0x68;
-    public static final byte END_BYTE = 0x13;
+    public static final byte END_BYTE = 0x16;
     public static final boolean IS_ADD_ENTER = false;
     public static final MessageTypeEnum MESSAGE_TYPE = MessageTypeEnum.STRING;
 }
